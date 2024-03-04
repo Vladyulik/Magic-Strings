240b3731103ec36fbb005a7b46b34bd3be7c2048
vladyulik <vanya123wow321@gmail.com>
2024-03-04 22:48:42 +0200

    Remove for-of loop for simplicity

--- main.js
+++ main.js
@@ -3,17 +3,11 @@
 const fs = require('fs');
 
 function convertMarkdownToHTML(markdown) {
-  let html = '';
-  const lines = markdown.split('\n');
+  const formattedText = markdown
+    .replace(/(```\n)([^`]*)\1/g, (match, symbol, content) => content)
+    .replace(/(\*\*|_|`)([^ \n]+.*[^ \n]+)\1/g, (match, symbol, content) => content);
 
-  for (const line of lines) {
-    html += line
-      .replace(/(\*\*|_|`)([^ \n]+.*[^ \n]+)\1/g, (match, symbol, content) => content)
-      .replace(/(```\n)([.\n]*)\1/g, (match, symbol, content) => content);
-    html += '\n';
-  }
-
-  return html;
+  return formattedText;
 }
 
 fs.readFile('TextForTest.md', 'utf8', (err, data) => {
