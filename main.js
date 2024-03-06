5ea47fa51d38ebfdddbf5c960baf8a12ad39d3ad
vladyulik <vanya123wow321@gmail.com>
2024-03-06 03:17:39 +0200

    Add id system for preformatted text blocks

--- main.js
+++ main.js
@@ -10,12 +10,21 @@ const replacements = {
 };
 
 function convertMarkdownToHTML(markdown) {
+  const preformattedBlocks = {};
+
   const formattedText = markdown
     .replace(/(```\r?\n)([^`]*)\1/g, (match, symbol, content) =>
       replacements[symbol.trim()](content)
     )
+    .replace(/<pre>\n.*<\/pre>/gs, (match) => {
+      const id = `^^PRE^^${Object.keys(preformattedBlocks).length}^^`;
+      preformattedBlocks[id] = match;
+      return id;
+    })
     .replace(/(\*\*|_|`)([^ \r\n]+.*[^ \r\n]+)\1/g, (match, symbol, content) =>
       replacements[symbol](content)
+    )
+    .replace(/\^\^PRE\^\^.+\^\^/g, (match) => preformattedBlocks[match]
     );
 
   return formattedText;
