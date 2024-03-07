717688f10699577388a6752bfa224db5615130d1
vladyulik <vanya123wow321@gmail.com>
2024-03-08 00:31:16 +0200

    Change \b back to \s so regexes work properly
    This reverts commit 56b7630ccbafab60d01d8b2cfc9b1a6acb72e429.

--- app.js
+++ app.js
@@ -13,21 +13,21 @@ const replacements = {
 function validateMarkdown(markdown) {
   const badFormatting = {};
 
-  const nestedFormatting = [...markdown.matchAll(/\b(\*\*|_|`)(\*\*|_|`){1,}([^ \r\n]+.*[^ \r\n]+)\1\b/g)];
+  const nestedFormatting = [...markdown.matchAll(/\s(\*\*|_|`)(\*\*|_|`){1,}([^ \r\n]+.*[^ \r\n]+)\1\s/g)];
 
   for (const instance of nestedFormatting) {
     const [match] = instance;
     badFormatting[match.trim()] = 'Nested formatting is not allowed';
   }
 
-  const endlessFormatting = [...markdown.matchAll(/\b(\*\*|_|`)[^ *_`\r\n]+(?!.*\1).*\r?\n/g)];
+  const endlessFormatting = [...markdown.matchAll(/\s(\*\*|_|`)[^ *_`\r\n]+(?!.*\1).*\r?\n/g)];
 
   for (const instance of endlessFormatting) {
     const [match] = instance;
     badFormatting[match.trim()] = 'Endless formatting is not allowed';
   }
 
-  const startlessFormatting = [...markdown.matchAll(/(^|\n)[^(**)(_)(`)]*[^ *_`\r\n](\*\*|_|`)\b/g)];
+  const startlessFormatting = [...markdown.matchAll(/(^|\n)[^(**)(_)(`)]*[^ *_`\r\n](\*\*|_|`)\s/g)];
 
   for (const instance of startlessFormatting) {
     const [match] = instance;
