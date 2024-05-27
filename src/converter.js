2ef3d95fcf839a504ac61533c4c62e19bee61291
vladyulik <vanya123wow321@gmail.com>
2024-05-27 14:02:16 +0300

    Fix converter so preformatted text works properly

--- src/converter.js
+++ src/converter.js
@@ -4,7 +4,7 @@ function convertMarkdown(markdown, conversionTable) {
   const preformattedBlocks = {};
 
   let formattedText = markdown
-    .replace(/(```\r?\n)([^`]*)\1/gs, (match) => {
+    .replace(/(```\r?\n)([^]*)\1/gs, (match) => {
       const id = `^^PRE^^${Object.keys(preformattedBlocks).length}^^`;
       preformattedBlocks[id] = match;
       return id;
@@ -14,7 +14,7 @@ function convertMarkdown(markdown, conversionTable) {
     )
     .replace(/\^\^PRE\^\^.+\^\^/g, (match) => preformattedBlocks[match]
     )
-    .replace(/(```\r?\n)([^`]*)\1/g, (match, symbol, content) =>
+    .replace(/(```\r?\n)([^]*)\1/g, (match, symbol, content) =>
       conversionTable[symbol.trim()](content)
     );
 
